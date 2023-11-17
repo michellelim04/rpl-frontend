a5374d630057de148737490196fa8a4c6de32a12
Michelle Lim <18221052@std.stei.itb.ac.id>
2023-11-17 18:06:08 +0900

    chore: initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Template from "@/components/template"
+
+export default function Home() {
+  return (
+    <Template>
+      <main className="min-h-screen align-middle justify-center flex flex-col">
+        <h1 className="text-center font-bold text-4xl">Rungkad.. entek entek an.. <br />  kelangan koe sing paling tak sayang..</h1>
+
+      </main>
+    </Template>
+  )
+}
