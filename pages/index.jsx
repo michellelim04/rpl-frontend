b3bd38468c139a3edee57c40b7edecc78e5a3b06
Michelle Lim <18221052@std.stei.itb.ac.id>
2023-11-21 22:39:45 +0900

    fix: index page spacing

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,35 +10,35 @@ export default function Home({ kelasMengemudi, propertyWebsite }) {
     <Template>
       <main className="min-h-screen px-14 py-5 bg-[#FFF6F6]">
         <h1 className="text-[#F875AA] font-extrabold text-4xl mt-5 mb-20 text-center">Sistem  Manajemen  Kursus  Mengemudi  RPL</h1>
-                
+
         <p>{propertyWebsite.description}</p>
-        
+
         <div className="flex justify-center items-center">
           <span className="text-[#F875AA] font-extrabold text-3xl text-center">Paket Kelas</span>
         </div>
-        
-        <div className={`max-h-fit px-28 mt-10 mx-auto max-w-[1270px] relative grid grid-cols-3 gap-x-8 gap-y-80`}>
+
+        <div className={`max-h-fit px-28 mt-10 mx-auto max-w-[1270px] relative grid grid-cols-3 gap-x-8 gap-y-8`}>
           {kelasMengemudi.map((row, index) => {
             return (
-            <div key={row.kelasMengemudiID}
-            className={`relative col-span-1 row-span-1 col-start-${(index % 3) + 1} row-start-${Math.floor(index / 3) + 1}`}>
-              <div className="w-[320px] h-[360px] left-0 top-0 absolute bg-white rounded-[15px] shadow relative flex flex-col justify-center items-center">
-                <div className="text-[#F875AA] text-xl font-extrabold">{row.namaKelas}</div>
-                <div className="text-center text-black text-base mt-8">Harga: Rp{row.hargaKelas}</div>
-                <div className="text-center text-black text-base mt-3">Total Jam Kursus: {2*row.jumlahSesi}</div>
-                <div className="text-center text-black text-base mt-3">Jumlah Sesi: {row.jumlahSesi}</div>
-                <div className="text-center text-black text-base mt-3">Nama Kendaraan: {row.namaKendaraan}</div>
-                <div className="text-center text-black text-base mt-3">Jenis Kendaraan: {row.jenisKendaraan}</div>
-                
-                <button onClick={(e) => {
-                  e.preventDefault();
-                  router.push({
-                    pathname: "/calonpelanggan/create",
-                    query: {kelasID: row.kelasMengemudiID, namaKelas: row.namaKelas},
-                  })
-                }} className="bg-[#AFDEFC] w-[126px] h-[53px] p-3 text-lg font-bold text-black rounded-2xl mt-8">Daftar</button>
-              </div>
-            </div>);
+              <div key={row.kelasMengemudiID}
+                className={`relative col-span-1 row-span-1 col-start-${(index % 3) + 1} row-start-${Math.floor(index / 3) + 1}`}>
+                <div className="w-[320px] h-[360px] left-0 top-0 bg-white rounded-[15px] shadow relative flex flex-col justify-center items-center">
+                  <div className="text-[#F875AA] text-xl font-extrabold">{row.namaKelas}</div>
+                  <div className="text-center text-black text-base mt-8">Harga: Rp{row.hargaKelas}</div>
+                  <div className="text-center text-black text-base mt-3">Total Jam Kursus: {2 * row.jumlahSesi}</div>
+                  <div className="text-center text-black text-base mt-3">Jumlah Sesi: {row.jumlahSesi}</div>
+                  <div className="text-center text-black text-base mt-3">Nama Kendaraan: {row.namaKendaraan}</div>
+                  <div className="text-center text-black text-base mt-3">Jenis Kendaraan: {row.jenisKendaraan}</div>
+
+                  <button onClick={(e) => {
+                    e.preventDefault();
+                    router.push({
+                      pathname: "/calonpelanggan/create",
+                      query: { kelasID: row.kelasMengemudiID, namaKelas: row.namaKelas },
+                    })
+                  }} className="bg-[#AFDEFC] w-[126px] h-[53px] p-3 text-lg font-bold text-black rounded-2xl mt-8">Daftar</button>
+                </div>
+              </div>);
           })}
         </div>
 
@@ -59,7 +59,7 @@ export default function Home({ kelasMengemudi, propertyWebsite }) {
 
       </main>
     </Template>
-    </>
+  </>
 }
 
 export const getServerSideProps = async () => {
