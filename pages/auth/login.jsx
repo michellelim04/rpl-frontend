a915d81b9bc777cc637179eff4a30c93d9603549
estherregina <18221086@std.stei.itb.ac.id>
2023-11-21 13:24:51 +0700

    fix: benerin tampilan log in

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -11,19 +11,18 @@ const Login = () => {
     <main className="min-h-screen px-14 py-7 bg-[#FFF6F6]">
 
     <div className="w-full mb-2">
-      <img className="w-[352px] h-[352px] left-[580px] top-[168px] absolute" src="/logo.png" />
-      <div className="w-[693px] h-[124px] left-[409px] top-[106px] absolute text-center text-pink-400 text-[40px] font-extrabold font-'Poppins'">Log In</div>
-      <div className="w-[130px] h-[33px] left-[427px] top-[559px] absolute text-pink-400 text-xl font-extrabold font-'Poppins'">Username</div>
-      <div className="w-[58px] h-[27px] left-[72px] top-[48px] absolute text-pink-400 text-xl font-extrabold font-'Poppins'">
+      <img className="w-[352px] h-[352px] left-[580px] top-[120px] absolute" src="/logo.png" />
+      <div className="w-[693px] h-[124px] left-[409px] top-[80px] absolute text-center text-pink-400 text-[40px] font-extrabold">Log In</div>
+      <div className="w-[130px] h-[33px] left-[427px] top-[505px] absolute text-pink-400 text-xl font-extrabold">Username</div>
+      <div className="w-[58px] h-[27px] left-[62px] top-[30px] absolute text-pink-400 text-xl font-extrabold">
         <span className="text-[#F875AA] font-bold text-2xl hover:cursor-pointer" onClick={(e) => {
               e.preventDefault()
               router.back()
             }}>Back</span>
       </div>
-      <div className="w-[130px] h-[33px] left-[427px] top-[654px] absolute text-pink-400 text-xl font-extrabold font-'Poppins'">Password</div>
+      <div className="w-[130px] h-[33px] left-[427px] top-[605px] absolute text-pink-400 text-xl font-extrabold">Password</div>
       <div className="w-[252px] h-[55px] left-[630px] top-[750px] absolute">
       </div>
-      <img className="w-[22px] h-[22px] left-[38px] top-[53px] absolute" src="/back.png" />
     </div>
 
       <form onSubmit={(e) => {
@@ -65,15 +64,17 @@ const Login = () => {
           toast.error("Something went wrong..")
         })
       }} >
-        <input type="text" placeholder=" Username" required className="w-4/12 h-14 left-[602px] top-[536px] absolute bg-white rounded-[15px] p-3 shadow border border-red-100" value={username} onChange={(e) => {
+        <input type="text" placeholder=" Username" required className="w-4/12 h-14 left-[602px] top-[490px] absolute bg-white rounded-[15px] p-3 shadow border border-red-100" value={username} onChange={(e) => {
               setUsername(e.target.value)
             }} />
-        <input type="password" placeholder=" Password" required className="w-4/12 h-14 left-[602px] top-[643px] absolute bg-white rounded-[15px] p-3 shadow border border-red-100" value={password} onChange={(e) => {
+        <input type="password" placeholder=" Password" required className="w-4/12 h-14 left-[602px] top-[590px] absolute bg-white rounded-[15px] p-3 shadow border border-red-100" value={password} onChange={(e) => {
           setPassword(e.target.value)
         }} />
-        <input type="submit" className="w-[252px] h-[55px] left-[630px] top-[750px] absolute w-[252px] h-[55px] left-0 top-0 absolute bg-pink-400 rounded-[15px] w-[168.56px] h-[29.40px] left-[41.72px] top-[11.38px] absolute text-center text-white text-2xl font-bold font-'Poppins' hover:cursor-pointer" value ={"Log In"}/>
+        <input type="submit" className="w-[252px] h-[55px] left-[630px] top-[700px] absolute w-[252px] h-[55px] left-0 top-0 absolute bg-pink-400 rounded-[15px] w-[168.56px] h-[29.40px] left-[41.72px] top-[11.38px] absolute text-center text-white text-2xl font-bold hover:cursor-pointer" value ={"Log In"}/>
 
       </form>
+      <div className="bg-[#FFF6F6] p-100 mb-20"> {/* Changed mb-20 to mb-4 for a smaller margin */}
+      </div>
     </main>
   )
 
